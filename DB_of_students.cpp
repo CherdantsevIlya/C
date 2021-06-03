5ecec837bab964c2dc208ad95e6ca19edc4dced5
pkari <pkari@student.21-school.ru>
2021-06-03 20:13:57 +0500

    first push

---
+++ DB_of_students.cpp
@@ -0,0 +1,248 @@
+﻿#include <iostream>
+#include "StringBuilderClass.h"
+#include "FileManagerClass.h"
+#include "ClassMenu.h"
+#include "StudentDBClass.h"
+#include "StudentClass.h"
+#include "ClassEdit.h"
+#include "list.hpp"
+using namespace std;
+
+List<string>::iterator mMin(List <string>* _lst) {
+    List<string>::iterator pos = _lst->begin();
+    List<string>::iterator mMinPos = _lst->begin();
+    string tmpString = *pos;
+    while (pos != _lst->end()) {
+        if (*pos > tmpString)
+            mMinPos = pos;
+        ++pos;
+    }
+    return mMinPos;
+
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
+    std::cout << "Курсовая работа запущена...\n";
+    StudentDBClass* sdb = new StudentDBClass();    
+    StudentDBClass* sdb1 = new StudentDBClass();
+    StudentDBClass* sdb2 = new StudentDBClass();
+    sdb->FileName = "DB.txt";
+    sdb->loadDataFromFile();
+
+    bool debug = false;
+    if (debug) {
+
+
+        //Сортировка строк
+        List <string> lst = { "Яна" , "Алина Рогова", "Ксюша","Данила", "Алексей" ,"Армен" };
+        //Просмотреть список
+        for (auto item : lst) {
+            cout << item << endl;
+        }
+        List <string> sortedLst;
+        sortedLst.clear();
+        //cout << *mMin(&lst);
+        while (!lst.empty()) {
+            sortedLst.push_front(*mMin(&lst));
+            lst.erase(mMin(&lst));
+        }
+        //Просмотреть отсортированный список
+        cout << endl << "------------" << endl;
+        for (auto item : sortedLst) {
+            cout << item << endl;
+        }
+    }
+    else {
+    
+    //cout << sdb->GetRecordCount() << endl;
+    ClassMenu* mainMenu = new ClassMenu();
+    mainMenu->addTitleItem("Главное меню");
+    mainMenu->addItem("Просмотреть список студетов (удалить или изменить данные)"); //0
+    mainMenu->addItem("Добавить данные о студенте в БД"); //1
+    mainMenu->addItem("Сохранить БД студентов в файл"); //2
+    mainMenu->addItem("Выполнить вариант 50"); //3
+    mainMenu->addItem("Выход"); //4
+    int resultSelectedItem = 0;
+    int exitInt = 3;
+    ClassMenu* studentsMenu = new ClassMenu();
+    studentsMenu->addTitleItem("Список студентов");
+    int resultStudentSelectedItem = 1;
+    const int exitIntStudentMenu = 0;
+    StudentNode* sn;
+    StudentClass* st = new StudentClass();
+    ClassMenu* delStudentsMenu = new ClassMenu();
+    int curCount;
+    ClassEdit* ce = new  ClassEdit();
+    int startYear = 0;
+    int endYear = 0;
+    int year = 0;
+    StringBuilderClass* sb = new StringBuilderClass();
+    while (resultSelectedItem != exitInt) {
+        mainMenu->run();
+        resultSelectedItem = mainMenu->getSelectedItem();
+        ClassMenu* sexMenu = new ClassMenu();
+        int resultSelectedVar = 1;
+        const int exitVar = 3;
+        switch (resultSelectedItem) {
+        case 0:
+            resultStudentSelectedItem = 1;
+            while (resultStudentSelectedItem != exitIntStudentMenu) {
+                studentsMenu->eraseItem();
+                studentsMenu->addItem("Выход");
+                studentsMenu->addItem("Удалить данные о студенте");
+                /*sn = sdb->getInit();*/
+                for(int i=0;i<sdb->DataBase.size();i++)
+                { // добавить пункты меню ФИО студентов
+                    sn = &sdb->DataBase.at(i);
+                    string tmpString = sn->surName + " " + sn->name + " " + sn->middleName + " " + sn->group;
+                    studentsMenu->addItem(tmpString); //добавить в меню студентов
+                    //sn = sn->next;
+                }
+                studentsMenu->run();
+                resultStudentSelectedItem = studentsMenu->getSelectedItem();
+                if (resultStudentSelectedItem == exitIntStudentMenu) {
+                    break;
+                }
+                if (resultStudentSelectedItem == 1) //удаление данных о студенте
+                {
+                    delStudentsMenu->eraseAll();
+                    delStudentsMenu->addTitleItem("Выберите студента для удаления данных");
+                    delStudentsMenu->addItem("Выход");
+                    int resultDel = 1;
+                    const int exitDel = 0;
+                    for(int i=0; i<sdb->DataBase.size();i++)
+                    { // добавить пункты меню ФИО студентов
+                        sn = &sdb->DataBase.at(i);
+                        string tmpString = sn->surName + " " + sn->name + " " + sn->middleName + " " + sn->group;
+                        delStudentsMenu->addItem(tmpString); //добавить в меню студентов
+                        //sn = sn->next;
+                    }
+                    while (resultDel != exitDel) {
+                        delStudentsMenu->run();
+                        resultDel = delStudentsMenu->getSelectedItem();
+                        if (resultDel == exitDel) {
+                            break;
+                        }
+                        else {
+                            int num = resultDel - 1;
+                            sdb->DataBase.erase(std::next(sdb->DataBase.begin(), num));
+                            break;
+                        }
+                    }
+                }
+                if (resultStudentSelectedItem>1)
+                {
+                    int num = resultStudentSelectedItem - 2; //!
+                    sn = &sdb->DataBase.at(num);   
+                    string oldRecordСardNumber = "";
+                    oldRecordСardNumber = sn->recordСardNumber;
+                    st->editStudent(sn);
+
+                    if (sdb->getSameRecordСardNumber(sn->recordСardNumber)>1)
+                    {
+                        sn->recordСardNumber = oldRecordСardNumber;
+                        cout << "Ошибка введен номер зачетной книжки который уже есть в БД";
+                        _getch();
+                    }
+                }
+            }
+            //resultSelectedItem = exitInt;
+            break;
+        case 1:
+            sn = new StudentNode();
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 10; j++)
+                    sn->examsRecordsData[i][j].isEmpty = true;
+            st->editStudent(sn);
+            if (sdb->getSameRecordСardNumber(sn->recordСardNumber)>=1)
+            {
+                cout << "Ошибка введен номер зачетной книжки который уже есть в БД";
+                _getch();
+            }
+            else
+            {
+                sdb->DataBase.push_front(*sn);
+            }
+            break;        
+        case 2: //Сохранить в файл
+            sdb->saveDataToFile(sdb->FileName);
+            //sdb->saveDataToFile("d:\\db1.txt");
+            break;
+        case 3: //
+            ce->setLabel("Введите год поступления в ВУЗ. ");
+            startYear = ce->setDataInt(1900, 2021);
+            sexMenu->addTitleItem("Выберите пол: ");
+            sexMenu->addItem("Мальчик");
+            sexMenu->addItem("Девочка");
+            while (resultSelectedVar != exitVar) {
+                sexMenu->run();
+                cout << "Полный список студентов: " << endl;
+                sdb->updateAvrMarks();  //Перерасчитать поле средний балл 
+                sdb->printAllSurName_Name_MName_sYaear_avrMarks();
+                resultSelectedVar = sexMenu->getSelectedItem();
+                switch (resultSelectedVar) {
+                case 0:
+                    cout << "----------------------------------------------------------" << endl;
+                    sdb1->DataBase.clear();
+                    sdb2->DataBase.clear();
+                    for (auto item : sdb->DataBase)
+                    {
+                        sn = &item;
+                        if (item.startYear == startYear) {
+                            sdb1->DataBase.push_front(*sn);
+                            sdb1->sortByAvrMarks();
+                        }
+                        else {
+                            sdb2->DataBase.push_front(*sn);
+                            sdb2->sortByAvrMarks();
+                        }
+                    }
+                    cout << "Список студентов мужского пола, поступивших в ВУЗ в " + std::to_string(startYear) + " году: " << endl;
+                    sdb1->printAllSurName_Male();
+                    cout << "----------------------------------------------------------" << endl;
+                    cout << "Список студентов мужского пола,НЕ поступивших в ВУЗ в " + std::to_string(startYear) + " году: " << endl;
+                    sdb2->printAllSurName_Male();
+                    resultSelectedVar = exitVar;
+                    break;
+                case 1:
+                    cout << "----------------------------------------------------------" << endl;
+                    sdb1->DataBase.clear();
+                    sdb2->DataBase.clear();
+                    for (auto item : sdb->DataBase)
+                    {
+                        sn = &item;
+                        if (item.startYear == startYear) {
+                            sdb1->DataBase.push_front(*sn);
+                            sdb1->sortByAvrMarks();
+                        }
+                        else {
+                            sdb2->DataBase.push_front(*sn);
+                            sdb2->sortByAvrMarks();
+                        }
+                    }
+                    cout << "Список студентов женского пола, поступивших в ВУЗ в " + std::to_string(startYear) + " году: " << endl;
+                    sdb1->printAllSurName_Female();
+                    cout << "----------------------------------------------------------" << endl;
+                    cout << "Список студентов женского пола,НЕ поступивших в ВУЗ в " + std::to_string(startYear) + " году: " << endl;
+                    sdb2->printAllSurName_Female();
+                    resultSelectedVar = exitVar;
+                    break;
+                default:
+                    break;
+                }
+            }
+            break;
+        case 4:
+            resultSelectedItem = exitInt;
+            break;
+        default:
+            break;
+        }
+    }
+    //_getch();
+    }
+}
+
---
+++ StudentDBClass.h
@@ -0,0 +1,278 @@
+#pragma once
+#include "StudentClass.h"
+#include <iostream>
+#include "FileManagerClass.h"
+#include "list.hpp"
+using namespace std;
+class StudentDBClass : public FileManagerClass
+{
+public:
+	List <StudentNode> DataBase;
+	StudentDBClass() {
+		DataBase.clear();
+	}
+	~StudentDBClass() {
+		DataBase.clear();
+	}
+	void loadDataFromFile() {
+		string line;
+		int count = 0;
+		std::ifstream inFile(FileName); // окрываем файл для чтения
+		if (inFile.is_open())
+		{
+			bool isRecord = false;
+			StudentNode* sn = new StudentNode();
+			for (int i = 0; i < 9; i++)
+				for (int j = 0; j < 10; j++)
+					sn->examsRecordsData[i][j].isEmpty = true;  
+			int studentId = 0;
+			while (getline(inFile, line))
+			{
+				if (strcmp(startRecordString.c_str(), line.c_str()) == 0) {
+					isRecord = true;
+					continue;
+				}
+				if (strcmp(endRecordString.c_str(), line.c_str()) == 0) {
+					isRecord = false;
+					studentId++;
+					DataBase.push_front(*sn);
+					//Add(sn);  
+					//
+					//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+					/// add
+					for (int i = 0; i < 9; i++)
+						for (int j = 0; j < 10; j++)
+							sn->examsRecordsData[i][j].isEmpty = true;
+					continue;
+				}
+				if (isRecord) {
+
+					string valueName = getName(line);
+					if (strcmp(getType(line).c_str(), "str") == 0)
+					{
+						string value = getValueStr(line);
+						//cout << value << endl;
+						if (strcmp("surName", valueName.c_str()) == 0)
+							sn->surName = value;
+						if (strcmp("name", valueName.c_str()) == 0)
+							sn->name = value;
+						if (strcmp("middleName", valueName.c_str()) == 0)
+							sn->middleName = value;
+						if (strcmp("faculty", valueName.c_str()) == 0)
+							sn->faculty = value;
+						if (strcmp("department", valueName.c_str()) == 0)
+							sn->department = value;
+						if (strcmp("group", valueName.c_str()) == 0)
+							sn->group = value;
+						if (strcmp("recordСardNumber", valueName.c_str()) == 0)
+							sn->recordСardNumber = value;
+						if (strcmp("birthDateString", valueName.c_str()) == 0)
+							sn->birthDateString = value;
+						// ExamsRecords
+						for (int i = 0; i < 9; i++)
+							for (int j = 0; j < 10; j++) {
+								//examsResults_0_2_n
+											// i j
+								string testNameString = "";
+								testNameString = testNameString + "examsResults_" + std::to_string(i) + "_" + std::to_string(j) + "_n";
+								string testMarkString = "";
+								testMarkString = testMarkString + "examsResults_" + std::to_string(i) + "_" + std::to_string(j) + "_m";
+								if (strcmp(testNameString.c_str(), valueName.c_str()) == 0) {
+									sn->examsRecordsData[i][j].name = value;
+									sn->examsRecordsData[i][j].isEmpty = false;
+								}
+								if (strcmp(testMarkString.c_str(), valueName.c_str()) == 0) {
+									sn->examsRecordsData[i][j].mark = atoi(value.c_str());
+									sn->examsRecordsData[i][j].isEmpty = false;
+								}
+							}
+					}
+					else // Тогда явно int но надо проверить
+					{
+						int value = getValueInt(line);
+						if (strcmp("id", valueName.c_str()) == 0)
+							sn->id = studentId;  // сейчас здесь порядковый номер записи в файле
+						//должно быть value хорошо бы, но ладно;) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+						if (strcmp("startYear", valueName.c_str()) == 0)
+							sn->startYear = value;
+						if (strcmp("sex", valueName.c_str()) == 0)
+							if (value == 0)
+								sn->sex = false;
+							else
+								sn->sex = true;
+					}
+				}
+			}
+			inFile.close();     // закрываем файл
+		}
+		else{
+			cout << " \n Файл БД не найден \n";
+			_getch();
+		}
+		inFile.close();
+	}
+	void saveDataToFile(string inFileName) {
+		std::ofstream outFile;          // поток для записи
+		ifstream iff(inFileName); //если файл есть удаляем
+		if (iff.bad() == false)  
+		{
+			iff.close();
+			if (remove(inFileName.c_str())) {
+				printf("Error removing file");
+				_getch();
+			}
+			
+		}
+        outFile.open(inFileName, std::ios::app); // окрываем файл для записи
+		// outFile.open(FileName, std::ios::app); // окрываем файл для записи
+        if (outFile.is_open())
+        {
+            StudentClass st = StudentClass();
+			int recordsCount = getRecordCount();
+			StudentNode* sn;
+			for (int i = 0; i < recordsCount;i++) {
+            //st.addRusakov();
+				outFile << startRecordString << std::endl;
+				sn = &DataBase.at(i);
+				st.UpdateMasString(sn);
+				while (!st.stringList.empty()) {
+					outFile << st.stringList.at(0) << std::endl;
+					st.stringList.erase(st.stringList.begin());
+				}
+				outFile << endRecordString << std::endl;
+			}
+            
+        }
+        outFile.close();
+	}
+	void setData(StudentNode* tNode, StudentNode* st) {
+		tNode->surName = st->surName;
+		tNode->name = st->name;
+		tNode->middleName = st->middleName;
+		tNode->faculty = st->faculty;
+		tNode->department = st->department;
+		tNode->group = st->group;
+		tNode->recordСardNumber = st->recordСardNumber;
+		tNode->sex = st->sex;
+		tNode->startYear = st->startYear;
+		tNode->birthDateString = st->birthDateString;
+		tNode->avrMarks = st->avrMarks; ///!!!!
+		// //по аналогии
+		//ExamsRecords data[9][10];
+		for(int i=0;i<9;i++)
+			for (int j = 0; j < 10; j++) {
+				tNode->examsRecordsData[i][j].isEmpty = st->examsRecordsData[i][j].isEmpty;
+				tNode->examsRecordsData[i][j].name = st->examsRecordsData[i][j].name;
+				tNode->examsRecordsData[i][j].mark = st->examsRecordsData[i][j].mark;
+			}
+		//tNode->isNULL = false;
+	}
+	//bool Add(StudentNode *st) {
+	//	if (!head)
+	//	{
+	//		head = new StudentNode();
+	//		//head->surName = st->surName;
+	//		setData(head, st);
+	//		//head->data = st;
+	//		head->next = NULL;
+	//		count++;
+	//		return true;
+	//	}
+	//	else
+	//	{
+	//		StudentNode* tmp = new StudentNode();
+	//		setData(tmp, st); //!!!!
+	//		//tmp->data = st;
+	//		tmp->next = head;
+	//		head = tmp;
+	//		count++;
+	//		return true;
+	//	}
+	//	return false;
+	//}
+	//StudentNode* getInit() {
+	//	return getNextNode = head;
+	//}
+	//int getCount() {
+	//	return count;
+	//}
+	void printAllSurName() {
+		for (int i = 0; i < DataBase.size(); i++) {
+			cout << DataBase.at(i).surName << endl;
+		}
+	}
+
+	int getRecordCount() {
+		return DataBase.size();
+	};
+
+	int getSameRecordСardNumber(string inString) {
+		int count = 0;
+		for (auto item : DataBase) {
+			if (item.recordСardNumber == inString)
+				count++;
+		}
+		return count;
+	}
+	void updateAvrMarks() {
+
+		StudentClass* stud = new StudentClass();
+		for (int i = 0; i < DataBase.size(); i++) { 
+			DataBase.at(i).avrMarks= stud->getAvrMarks(&DataBase.at(i));
+		}
+		delete stud;
+	}
+	void printAllSurName_Name_MName_sYaear_avrMarks() {
+		StringBuilderClass* sb = new StringBuilderClass();
+		for(auto item:DataBase){
+			cout << item.surName + " " + item.name + " " + item.middleName + " " + std::to_string(item.startYear) + " " + std::to_string(item.avrMarks) << endl;
+		}
+		delete sb;
+	}
+	void printAllSurName_Female() {
+		StudentNode* sn;
+		StringBuilderClass* sb = new StringBuilderClass();
+		for (auto item : DataBase) {
+			sn = &item;
+			if (sn->sex == 0) {
+				cout << item.surName + " " + item.name + " " + item.middleName + " " + std::to_string(item.startYear) + " " + std::to_string(item.avrMarks) << endl;
+			}
+		}
+		delete sb;
+	}
+	void printAllSurName_Male() {
+		StudentNode* sn;
+		StringBuilderClass* sb = new StringBuilderClass();
+		for (auto item : DataBase) {
+			sn = &item;
+			if (sn->sex == 1) {
+				cout << item.surName + " " + item.name + " " + item.middleName + " " + std::to_string(item.startYear) + " " + std::to_string(item.avrMarks) << endl;
+			}
+		}
+		delete sb;
+	}
+
+	List <StudentNode>::iterator getMaxAvrMarks() {
+		List<StudentNode>::iterator pos = DataBase.begin();
+		List<StudentNode>::iterator mMinPos = pos;
+		while (pos != DataBase.end()) {
+			if ((*pos).avrMarks > (*mMinPos).avrMarks)
+				mMinPos = pos;
+			++pos;
+		}
+		return mMinPos;
+	}
+	void sortByAvrMarks() {
+
+		List<StudentNode>::iterator pos = getMaxAvrMarks();
+		List <StudentNode> sortedLst;
+		while (!DataBase.empty()) {
+			sortedLst.push_front(*getMaxAvrMarks());
+			DataBase.erase(getMaxAvrMarks());
+		}
+		for (auto item : sortedLst) 
+			DataBase.push_front(item);
+	}
+};
+
+
